c93b0333464dbce8444d51ebe07645551998b4a4
Alexey Nurmukhametov <oleshka@ispras.ru>
2019-12-14 01:10:14 +0300

    Add option for run only one test binary --binary

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from os.path import join, splitext, basename, isdir
+from os.path import join, splitext, basename, isdir, dirname
 from os import listdir, environ, getcwd
 from argparse import ArgumentParser
 from sys import exit
@@ -55,6 +55,8 @@ parser.add_argument("-a", "--arch", type=str, default="x86",
                     help="The target architecture of framework.")
 parser.add_argument("-c", "--check-only", action='store_true', default=False,
                     help="Only check chains generated previously")
+parser.add_argument("-b", "--binary", type=str,
+                    help="Run particular binary e.g. openbsd-62/ac.bin")
 args = parser.parse_args()
 
 environ["PYTHONPATH"] = getcwd()
@@ -71,17 +73,21 @@ else:
     tools = [args.tool]
 
 test_suites = {}
-test_suites["synthetic"] = SYNTHETIC_VULN_DIR
-if not args.synthetic:
-    if args.real_life:
-        if args.real_life not in reallife_test_suites:
-            print("Incorrect real life test set. Choose one of:")
-            print(" ".join(reallife_test_suites))
-            exit(1)
-        test_suites[args.real_life] = join(REALLIFE_VULN_DIR, args.real_life)
-    else:
-        for t in reallife_test_suites:
-            test_suites[t] = join(REALLIFE_VULN_DIR, t)
+if args.binary:
+    test_suite = dirname(args.binary)
+    test_suites[test_suite] = join(REALLIFE_VULN_DIR, test_suite)
+else:
+    test_suites["synthetic"] = SYNTHETIC_VULN_DIR
+    if not args.synthetic:
+        if args.real_life:
+            if args.real_life not in reallife_test_suites:
+                print("Incorrect real life test set. Choose one of:")
+                print(" ".join(reallife_test_suites))
+                exit(1)
+            test_suites[args.real_life] = join(REALLIFE_VULN_DIR, args.real_life)
+        else:
+            for t in reallife_test_suites:
+                test_suites[t] = join(REALLIFE_VULN_DIR, t)
 
 results = {}
 for exp_type in exploit_types:
@@ -95,13 +101,16 @@ proc_pool = Pool(n_core)
 for tool in tools:
     for exploit_type in exploit_types:
         for test_suite_name, test_suite_dir in test_suites.items():
-            print("=== Tool '{}' === Exp. type '{}' === Test suite '{}'"
-                  .format(tool, exploit_type, test_suite_name))
-            tests = list_tests(test_suite_dir)
+            if args.binary:
+                tests = [join(test_suite_dir, basename(args.binary))]
+            else:
+                print("=== Tool '{}' === Exp. type '{}' === Test suite '{}'"
+                      .format(tool, exploit_type, test_suite_name))
+                tests = list_tests(test_suite_dir)
             passed = 0
             current_id = 1
-            args = [(test, tool, exploit_type) for test in tests]
-            for r in proc_pool.imap(run_test, args):
+            run_test_args = [(test, tool, exploit_type) for test in tests]
+            for r in proc_pool.imap(run_test, run_test_args):
                 is_passed, bstdout = r
                 if bstdout:
                     stdout = bstdout.decode()
@@ -109,9 +118,10 @@ for tool in tools:
                 passed += is_passed
                 current_id += 1
             results[exploit_type][tool][test_suite_name] = (passed, len(tests))
-            print("--- Result --- {} {} {} : {} / {} (passed/all)"
-                  .format(tool, exploit_type, test_suite_name,
-                          passed, len(tests)))
+            if not args.binary:
+                print("--- Result --- {} {} {} : {} / {} (passed/all)"
+                      .format(tool, exploit_type, test_suite_name,
+                              passed, len(tests)))
 
 print("--== Overall results summary ==--")
 print(results)
