a4e10bd485c07bde3694420fbc3704ee9f5effbb
Alexey Vishnyakov <vishnya@ispras.ru>
2022-06-09 20:32:34 +0300

    Fix Docker build

--- run.sh
+++ run.sh
@@ -11,7 +11,7 @@ docker run --rm -t -v `pwd`:/rop-benchmark/ \
   --name="rop-benchmark" \
   -e PYTHONUNBUFFERED=1 -e PYTHONPATH=/rop-benchmark \
   rop-benchmark \
-  bash -c "cd /rop-benchmark && python3 /rop-benchmark/run.py $*" &
+  /bin/bash -c "cd /rop-benchmark && python3 /rop-benchmark/run.py $*" &
 
 child=$!
 wait "$child"
